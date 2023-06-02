2817002e33e4a719d47c99f55b30d6558450b210
lsr <lsrprntr@gmail.com>
2023-06-02 14:55:11 +1000

    ctest

--- cstuff/ctest.c
+++ cstuff/ctest.c
@@ -6,4 +6,20 @@ int main(void)
     char name
     name = "What is your name?";
     printf("Hello, %s\n",name);
+    int x = 1;
+    int y = 2;
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
+    
 }
+
