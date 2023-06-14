f2c4e5e76012115634fe59cd3846cc5365c09704
lsr <lsrprntr@gmail.com>
2023-06-14 11:32:20 +1000

    notes c struct

--- cstuff/typedef.c
+++ cstuff/typedef.c
@@ -10,4 +10,13 @@ person;
 int main(void)
 {
     person contact[2];
+    contact[0].name = "David";
+    contact[0].number = "+61422684635";
+
+    person list[1];
+    // You can copy struct to struct
+    list[0] = contact[0];
+
+    printf("%s %s\n",list[0].name,list[0].number);
+
 }
