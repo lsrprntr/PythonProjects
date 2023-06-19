f490e687b6a114243c8001826c4d4ebd4117f79e
lsr <lsrprntr@gmail.com>
2023-06-19 13:46:03 +1000

    fileio

--- cstuff/fileio.c
+++ cstuff/fileio.c
@@ -7,7 +7,9 @@ int main(void)
     // FILE type | pointer | targetfile | read-mode
     FILE *input = fopen("hello.txt", "r");
     /* 
-    fopen essentially finds the address of the file 
-
+    fopen essentially finds the address of the file;
+    it is assigned to pointer input;
     */
+
+   
 }
