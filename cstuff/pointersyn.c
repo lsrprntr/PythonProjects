d034eaece9c7358f1f3efa3906d76f4d7636067e
lsr <lsrprntr@gmail.com>
2023-06-22 15:01:58 +1000

    pointer prac

--- cstuff/pointersyn.c
+++ cstuff/pointersyn.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *ptr;
+}
+node;
 
 int main(void)
 {
-    
+    node *n = malloc(sizeof(node));
+    node *p = malloc(sizeof(node));
+    node *q = malloc(sizeof(node));
+
+
+    n->number = 1;
+    n->ptr = p;
+
+    // OR
+
+    (*n).number = 2;
+    (*n).ptr = q;
+
+
+    return 0;
 }
