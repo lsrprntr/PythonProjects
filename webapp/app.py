ce3ff4ed439655d3704da8be8cadbbd3a169731b
lsr <lsrprntr@gmail.com>
2023-07-14 08:53:22 +1000

    notes

---
+++ webapp/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def input():
+    '''
+    if name in request.args:
+        name = request.args["name"]
+    else:
+        name = "world"
+    '''
+    name = request.args.get("name","world")
+    return render_template("index.html", name = name)
