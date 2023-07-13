4a75bb66a0a29eff7701081c17d98cd73ceb8313
lsr <lsrprntr@gmail.com>
2023-07-14 09:00:42 +1000

    notes

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flaskk_session import Session
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def input():
     '''
     if name in request.args:
@@ -13,3 +13,12 @@ def input():
     '''
     name = request.args.get("name","world")
     return render_template("index.html", name = name)
+
+@app.route("/registrants", methods=["POST"])
+def registrants():
+    if request.method =="POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("layout.html", name = name)
+
+
